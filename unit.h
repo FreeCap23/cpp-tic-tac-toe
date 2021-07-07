cd9867ebf74ad64997c74402ec8533d52483d2e7
FreeCap23 <dionisiestratulat@gmail.com>
2021-07-07 11:19:27 +0300

    - Render function works

--- unit.h
+++ unit.h
@@ -1,1 +1,18 @@
-class Unit;
+class Unit{
+	private:
+		char state;
+
+	public:
+		Unit(){
+			state = ' ';
+		}
+
+		void changeState(char newState){
+			if(state == ' ')
+				state = newState;
+		}
+
+		char getState(){
+			return state;
+		}
+};
