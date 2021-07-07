4885fae8235995dc5b6a34d58ac1b3b9d9fffc7d
FreeCap23 <dionisiestratulat@gmail.com>
2021-07-07 12:54:03 +0300

    Working build. Does not have winning conditions.

---
+++ render.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "unit.h"
+Unit unitArray[3][3];
+void render() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (j == 2)
+                std::cout << "[" << unitArray[i][j].getState() << "]"
+                          << std::endl;
+            else
+                std::cout << "[" << unitArray[i][j].getState() << "]";
+        }
+    }
+}
